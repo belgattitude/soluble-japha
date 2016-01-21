13b127bf8f4f6747ac3f104b2aa81deaad264b34
Sébastien Vanvelthem <belgattitude@gmail.com>
2016-01-21 21:58:27 +0100

    Increased memory limit

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -7,6 +7,7 @@ if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
 }
 
 ini_set('error_reporting', E_ALL | E_STRICT);
+ini_set('memory_limit', '256M');
 $baseDir = dirname(__DIR__);
 require_once(dirname(__FILE__) . '/SolubleTestFactories.php');
 
