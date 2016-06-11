537d7d3b54906a70c1358028803ffa2b019e73a5
Sébastien Vanvelthem <belgattitude@gmail.com>
2016-06-11 13:40:23 +0200

    Removed obsolete message

--- src/Soluble/Japha/Bridge/Driver/Pjb62/SimpleHttpTunnelHandler.php
+++ src/Soluble/Japha/Bridge/Driver/Pjb62/SimpleHttpTunnelHandler.php
@@ -105,7 +105,6 @@ class SimpleHttpTunnelHandler extends SimpleHttpHandler
     {
         if (!$socket) {
             $message  = " Could not connect to the JEE server {$this->ssl}{$this->host}:{$this->port}. Please start it.";
-            $message .= java_checkCliSapi() ? " Or define('JAVA_HOSTS',9267); define('JAVA_SERVLET',false); before including 'Java.inc' and try again. Error message: $errstr ($errno)\n" : " Error message: $errstr ($errno)\n";
             throw new Exception\ConnectException(__METHOD__ . $message);
         }
     }
